7a2d2c7f53a78f926245ac234a8249253699cf84
rayast17 <72138164+rayast17@users.noreply.github.com>
2020-12-06 18:54:33 +0200

    Add files via upload

---
+++ MyIntegerTest.java
@@ -0,0 +1,15 @@
+package domrab;
+
+public class MyIntegerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyInteger i = new MyInteger(17);
+		System.out.println(i.getValue());
+		System.out.println(i.equals(17));
+		
+
+
+	}
+
+}
---
+++ MyPointTest.java
@@ -0,0 +1,14 @@
+package domrab;
+
+public class MyPointTest {
+
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		
+		MyPoint a = new MyPoint();
+		MyPoint b = new MyPoint(10, 30.5);
+		System.out.println(a.distance(b));
+
+	}
+
+}
